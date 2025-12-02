98cd5395bb0f17a612cb25f68fc10fd86b44e7bf
JMorais <joaquim.morais-faria@outlook.com>
2025-12-02 16:28:54 +0100

    Modification de la page d'accueil, modification des couleurs définitives primary et secondary, installation de tailwind.
    Création du formulaire de contact, ajout de plusieurs todo pour accéder a l'envoi et pouvoir recevoir les mails sur la bonne adresse !!
    
    RAF : Page Prestation(?), page Services(?)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14213d",
+        secondary: "#F6A728",
+      },
+    },
+  },
+  plugins: [],
+};
